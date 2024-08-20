6023ee0c9bd43196ca383a8c91d8dfe99eea322e
DENZEL OMONDI <omondidenzel@DENZELs-MacBook-Pro.local>
2024-08-20 08:44:14 +0200

    Added new feature in etl.py file

--- DataExtraction/etl.py
+++ DataExtraction/etl.py
@@ -4,6 +4,12 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import config
+import sys
+import logging 
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+log = logging.getLogger(__name__)
+
 
 data_list = []
 
@@ -11,42 +17,47 @@ def get_data():
     for country in config.list_of_countries['countries']:
         # Define the URL
         url = f'{config.url}{country}'
+        log.info("Extracting -> {}".format(url))
 
-        # Call the Apo
-        fetch_page = requests.get(url)
+        # Call the Api
+        try:
+            fetch_page = requests.get(url)
+        except Exception as e:
+            log.info('Error --> {}'.format(e))
+            
+        if fetch_page.status_code == 200:
+            soup = BeautifulSoup(fetch_page.content, 'html.parser')
 
-        soup = BeautifulSoup(fetch_page.content, 'html.parser')
+            data_dict = {}
 
-        data_dict = {}
+            data_dict['Country'] = soup.css.select('.fn.org.country-name')[0].get_text()
+            data_dict['Latitude'] = soup.css.select('.latitude')[0].get_text()
+            data_dict['Longitude'] = soup.css.select('.longitude')[0].get_text()
 
-        data_dict['Country'] = soup.css.select('.fn.org.country-name')[0].get_text()
-        data_dict['Latitude'] = soup.css.select('.latitude')[0].get_text()
-        data_dict['Longitude'] = soup.css.select('.longitude')[0].get_text()
+            find_td_element = soup.find('td', class_='infobox-data')
+            data_dict['Capital'] = find_td_element.find('a').text
 
-        find_td_element = soup.find('td', class_='infobox-data')
-        data_dict['Capital'] = find_td_element.find('a').text
+            # data_dict['Populatiom_Estimate'] = soup.fin()
+            # data_dict['GDP'] = 
 
-        # data_dict['Populatiom_Estimate'] = soup.fin()
-        # data_dict['GDP'] = 
+            data_list.append(data_dict)
+            
+            # create empty DataFrame 
+            df = pd.DataFrame(data_list)
 
-        data_list.append(data_dict)
-        
-        # create empty DataFrame 
-        df = pd.DataFrame(data_list)
-
-        # Transformation
-        def removeSymbolsNumber(data_symbol):
-            data_symbol = re.sub(r'\[\d+\]', '', data_symbol)
-            return data_symbol 
-        
-        def removeNumericalValue(data_numerical):
-            data_numerical = re.sub('[^A-Za-z]+', ' ', data_numerical)
-            return data_numerical 
-        
-        df.columns = df.columns.str.lower()
-        df['country'] = df['country'].apply(removeSymbolsNumber)
-        df['capital'] = df['capital'].apply(removeNumericalValue)
-        
+            # Transformation
+            def removeSymbolsNumber(data_symbol):
+                data_symbol = re.sub(r'\[\d+\]', '', data_symbol)
+                return data_symbol 
+            
+            def removeNumericalValue(data_numerical):
+                data_numerical = re.sub('[^A-Za-z]+', ' ', data_numerical)
+                return data_numerical 
+            
+            df.columns = df.columns.str.lower()
+            df['country'] = df['country'].apply(removeSymbolsNumber)
+            df['capital'] = df['capital'].apply(removeNumericalValue)
+            
 
 
     print(df.head())
