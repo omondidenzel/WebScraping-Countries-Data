b86c09c72bc8f07e23796a087f6b944e3ba1d7b2
DENZEL OMONDI <omondidenzel@DENZELs-MacBook-Pro.local>
2024-08-21 21:27:30 +0200

    Added more countries

--- DataExtraction/config.py
+++ DataExtraction/config.py
@@ -1,6 +1,5 @@
 url = 'https://en.wikipedia.org/wiki/'
 
 list_of_countries =  {
-    'countries': ['Kenya', 'Rwanda', 'Uganda', 'Ethiopia', 'Ghana']
-                  
+    'countries': ['Kenya', 'Rwanda', 'Uganda', 'Ethiopia', 'Ghana', 'Malawi', 'Israel', 'Japan']                
 }
