96a0aa7654c088912b431903a90df057362c7a66
DENZEL OMONDI <omondidenzel@DENZELs-MacBook-Pro.local>
2024-08-22 18:26:07 +0200

    Added posting to DB script

--- DataExtraction/config.py
+++ DataExtraction/config.py
@@ -1,5 +1,17 @@
+from sqlalchemy import create_engine
+import logging, sys
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+log = logging.getLogger(__name__)
+
 url = 'https://en.wikipedia.org/wiki/'
 
 list_of_countries =  {
-    'countries': ['Kenya', 'Rwanda', 'Uganda', 'Ethiopia', 'Ghana', 'Malawi', 'Israel', 'Japan']                
+    'countries': ['Kenya', 'Rwanda', 'Uganda', 'Ethiopia', 'Ghana', 'Malawi', 'Israel', 'Japan', 'France', 'India', 'USA']                
 }
+
+def save_toDB():
+    URL_DATABASE = 'postgresql://postgres:ya!e3ekafE@localhost:5450/denzel'
+    return create_engine(URL_DATABASE)
+
+engine = save_toDB() 
